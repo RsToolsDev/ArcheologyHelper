6a49d36f979187feac14d84fc99130286c79301c
joshuashaver <josh.shaver@ymail.com>
2020-04-12 19:58:45 -0500

    added divination lvl requirement to the tier column of the porter cost module

--- porter-cost-analyzer.js
+++ porter-cost-analyzer.js
@@ -3,7 +3,7 @@ function PORTER_COST_CATEGORY_MAPPINGS() {
     this.HTML_TABLE_OUTPUT = '#porter-cost-table';
     this.HTML_STATE_OUTPUT = '#porter-cost-state';
     this.TABLE_HEADER = "<div class='pt-2 row font-weight-bold text-uppercase text-center'>"
-                                                +    "<div class='col-1'>Tier</div>"
+                                                +    "<div class='col-1'>Tier (lvl) </div>"
                                                 +    "<div class='col-4 text-left pl-5'>Energy (Price)</div>"
                                                 +    "<div class='col-3 text-left'>Necklace Needed (price)</div>"
                                                 +    "<div class='col-2'>Charges (Price/Charge)</div>"
@@ -25,12 +25,12 @@ function PORTER_COST_CATEGORY_MAPPINGS() {
 
 
  var porters = [
-    { "tier":"Tier 1", "charges":"5", "energy":"pale energy", "energyAmount":30, "jewelry":"sapphire necklace"},
-    { "tier":"Tier 2", "charges":"10", "energy":"bright energy", "energyAmount":35, "jewelry":"sapphire necklace"},
-    { "tier":"Tier 3", "charges":"15", "energy":"sparkling energy", "energyAmount":40, "jewelry":"emerald necklace"},
-    { "tier":"Tier 4", "charges":"20", "energy":"vibrant energy", "energyAmount":45, "jewelry":"emerald necklace"},
-    { "tier":"Tier 5", "charges":"25", "energy":"radiant energy", "energyAmount":60, "jewelry":"ruby necklace"},
-    { "tier":"Tier 6", "charges":"30", "energy":"incandescent energy", "energyAmount":80, "jewelry":"diamond necklace"}
+    { "tier":"I (6)", "charges":"5", "energy":"pale energy", "energyAmount":30, "jewelry":"sapphire necklace"},
+    { "tier":"II (28)", "charges":"10", "energy":"bright energy", "energyAmount":35, "jewelry":"sapphire necklace"},
+    { "tier":"III (48)", "charges":"15", "energy":"sparkling energy", "energyAmount":40, "jewelry":"emerald necklace"},
+    { "tier":"IV (68)", "charges":"20", "energy":"vibrant energy", "energyAmount":45, "jewelry":"emerald necklace"},
+    { "tier":"V (88)", "charges":"25", "energy":"radiant energy", "energyAmount":60, "jewelry":"ruby necklace"},
+    { "tier":"VI (99)", "charges":"30", "energy":"incandescent energy", "energyAmount":80, "jewelry":"diamond necklace"}
 ]
 
 var context = new PORTER_COST_CATEGORY_MAPPINGS;
@@ -61,16 +61,16 @@ async function getPriceDependencies(){
         let priceOfEnergy = castToNumericFormat(porter.energyPrice) * porter.energyAmount;
         let totalPrice = priceOfEnergy + castToNumericFormat(porter.jewelryCost);
         let pricePerCharge = Math.ceil(totalPrice/porter.charges);
-        tableData += addRowInHTML(addColumnInHTML(porter.tier, 'col-1') 
+        tableData += addRowInHTML(addColumnInHTML(porter.tier, 'col-1 my-2') 
             + addColumnInHTML(addImageInHTML(porter.energyIcon) + " " 
                 + porter.energyAmount + " x " 
                 + porter.energy.replace(' energy', '')
-                + " (" + priceOfEnergy + " gp) ", 'col-4', "text-left")
+                + " (" + priceOfEnergy + " gp) ", 'col-4 my-2', "text-left")
             + addColumnInHTML(addImageInHTML(porter.jewelryIcon) + " " 
                 + porter.jewelry.replace(' necklace', '')
-                + " (" + porter.jewelryCost + " gp) " , 'col-3', 'text-left')
-            + addColumnInHTML(porter.charges + " (" + pricePerCharge + " gp) " , 'col-2')
-            + addColumnInHTML(pricePerCharge * .5 + "K", 'col-2'));
+                + " (" + porter.jewelryCost + " gp) " , 'col-3 my-2', 'text-left')
+            + addColumnInHTML(porter.charges + " (" + pricePerCharge + " gp) " , 'col-2 my-2')
+            + addColumnInHTML(pricePerCharge * .5 + "K", 'col-2 my-2'));
     });
     $(context.HTML_TABLE_OUTPUT).append(tableData);
     setModuleState("#porter-cost-state", "DONE")
